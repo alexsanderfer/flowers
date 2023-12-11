99fec140ca92b2c9e0defbcbde0632bb16a47c50
Alexsander <afgoueia90@gmail.com>
2023-12-11 18:00:04 +0000

    Initial commit

---
+++ flowers/main.js
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2023. Created by Alexsander at 12/11. All rights reserved.
+ * GitHub: https://github.com/alexsanderfer/
+ * Portfolio: https://alexsanderfer.netlify.app/
+ */
+
+onload = () =>{
+        document.body.classList.remove("container");
+};
